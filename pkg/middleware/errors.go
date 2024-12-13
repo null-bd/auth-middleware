440417c2f37827280b65d0e0609f8f3e422977ab
FPVInMyScouter <messagetokarthi@gmail.com>
2024-12-14 02:47:16 +0530

    Update package name.

---
+++ pkg/middleware/errors.go
@@ -0,0 +1,12 @@
+package middleware
+
+import "errors"
+
+var (
+	ErrInvalidToken       = errors.New("invalid token")
+	ErrExpiredToken       = errors.New("token has expired")
+	ErrInsufficientScope  = errors.New("insufficient scope")
+	ErrMissingToken       = errors.New("missing token")
+	ErrInvalidConfig      = errors.New("invalid configuration")
+	ErrServiceUnavailable = errors.New("auth service unavailable")
+)
